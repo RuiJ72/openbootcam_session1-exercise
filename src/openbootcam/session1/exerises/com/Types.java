163a0c8862cf8297c4ecb6e61b3a67211d962d10
RuiJ72 <rlagos900@gmail.com>
2023-04-30 21:35:12 -0300

    OpenBootcamp_session1_exercise

---
+++ src/openbootcam/session1/exerises/com/Types.java
@@ -0,0 +1,34 @@
+package openbootcam.session1.exerises.com;
+
+public class Types {
+
+    public static void main(String[] args) {
+
+        // Main Data Types
+
+        // Integer Numerics
+        int integer = 200;
+        System.out.println("Example of an integer: " + integer);
+
+        float floating = 25.5f;
+        System.out.println("A float type with de value: " + floating);
+
+        double doubleNumber = 205.5d;
+        System.out.println("A decimal double type with de value: " + doubleNumber);
+
+        long longNumber = 1896525;
+        System.out.println("Type long example: " + longNumber);
+
+        boolean choiceTrue = true;
+        System.out.println("A boolean type boolean: " + choiceTrue);
+
+        boolean choiceFalse = false;
+        System.out.println("A boolean type boolean, false: " + choiceFalse);
+
+        String name = "Rui Jorge Lagos";
+        int age = 51;
+        System.out.println("My name is: " + name + " and I have  " + age + " years old!");
+
+
+    }
+}
